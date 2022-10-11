f2fffe510dee3448ecd30c72489d22b2dcaebf8c
KrystianKiznar <krystian.kiznar@gmail.com>
2022-10-11 09:49:24 +0200

    Add project files.

---
+++ FiveInARow/Player.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <utility>
+
+class Player
+{
+public:
+	std::string name;
+	char sign;
+
+	Player() = default;
+	std::pair<int, int> inputCoordinate();
+	void setPlayerName();
+	void setPlayerSign();
+};
